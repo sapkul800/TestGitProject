3a035209c97c975b903497310efb3afb0cd64dbc
Sapana <sapana_kulkarni@hotmail.com>
2019-10-19 14:46:42 +0530

    Added new code in the TestClass

--- TestGitJavaProject/src/TestClass.java
+++ TestGitJavaProject/src/TestClass.java
@@ -1,4 +1,12 @@
 
 public class TestClass {
+	
+		
+		public static void main(String[] args){
+			//print Hello World in the terminal window
+			System.out.println("Hello World");
+			System.out.println("Testing Eclipse - GitHub - Jenkins");
+			
+	}
+	}
 
-}
